931d12513ad4284b8029d081a40524952f832aa7
curiouscoder13 <172529323+curiouscoder13@users.noreply.github.com>
2024-08-23 19:22:47 +0800

    added functionality dynamic element rendering on ConferenceEvent.jsx, TotalCost.jsx, and others and minor CSS modifications

--- src/TotalCost.jsx
+++ src/TotalCost.jsx
@@ -1,27 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import PropTypes from "prop-types";
 import "./TotalCost.css";
 
 const TotalCost = ({ totalCosts, ItemsDisplay }) => {
-  
+  const total_amount = totalCosts.venue + totalCosts.av + totalCosts.meals;
 
   return (
     <div className="pricing-app">
       <div className="display_box">
         <div className="header">
-          <p className="preheading"><h3>Total cost for the event</h3></p>
+          <span className="preheading">
+            <h3>Total cost for the event</h3>
+          </span>
         </div>
         <div>
           <h2 id="pre_fee_cost_display" className="price">
-           
+            ${total_amount}
           </h2>
-         
-            <div>
-             
-            </div>
+          <div className="render_items">
+            <ItemsDisplay />
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
+TotalCost.propTypes = {
+  totalCosts: PropTypes.object.isRequired,
+  ItemsDisplay: PropTypes.func.isRequired,
+};
 export default TotalCost;
--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 // store.js
-import { configureStore } from '@reduxjs/toolkit';
-import venueReducer from './venueSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import venueReducer from "./venueSlice";
+import aVReducer from "./avSlice";
+import mealsReducer from "./mealsSlice";
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av: aVReducer,
+    meals: mealsReducer,
   },
 });
